bde14f9c15710dc4fec94271a20c69b00d6dede2
luong <89636817+hoangxuanluong@users.noreply.github.com>
2024-05-05 18:58:07 +0700

    mmmmm

--- src/main/java/com/javaspring/springmad/entity/UserCollectionDetail.java
+++ src/main/java/com/javaspring/springmad/entity/UserCollectionDetail.java
@@ -1,12 +1,6 @@
 package com.javaspring.springmad.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,20 +10,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class UserCollectionDetail {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User user;
-	
-	@ManyToOne
-	@JoinColumn(name = "collection_id")
-	private ExerciseCollection exerciseCollection;
-	
-	@Column()
-	private boolean state;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "collection_id")
+    private ExerciseCollection exerciseCollection;
+
+    @Column()
+    private boolean state;
 }
 
 
